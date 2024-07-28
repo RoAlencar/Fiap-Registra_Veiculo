fb8f977c313acb5ed663ffeb9d6c43a9b256fbec
Rodrigo de Alencar Xavier <roalencarx@gmail.com>
2024-07-28 09:58:48 -0300

    Ajustando teste-data-.sql

--- src/test/resources/teste-data.sql
+++ src/test/resources/teste-data.sql
@@ -1,8 +1,8 @@
 Insert into core_veiculo (ano,cor,data_atualizacao,data_cadastro,descricao,marca,modelo,numero_portas,placa,preco,quilometragem,status,tipo,tipo_combustivel)
-Values (1960,'Preto',null,null,'Carro','Ford','Mustang','DUAS','ABC123',50000,10000,'DISPONIVEL','ESPORTIVO','GASOLINA')
+Values (1960,'Preto',null,CURRENT_DATE,'Carro','Ford','Mustang','DUAS','ABC123',50000,10000,'DISPONIVEL','ESPORTIVO','GASOLINA')
 
 Insert into core_veiculo (ano,cor,data_atualizacao,data_cadastro,descricao,marca,modelo,numero_portas,placa,preco,quilometragem,status,tipo,tipo_combustivel)
-Values (1960,'Preto',null,null,'Carro','Ford','Mustang','DUAS','ABC123',50000,10000,'DISPONIVEL','ESPORTIVO','GASOLINA')
+Values (1960,'Preto',null,CURRENT_DATE,'Carro','Ford','Mustang','DUAS','ABC123',50000,10000,'DISPONIVEL','ESPORTIVO','GASOLINA')
 
 Insert into core_veiculo (ano,cor,data_atualizacao,data_cadastro,descricao,marca,modelo,numero_portas,placa,preco,quilometragem,status,tipo,tipo_combustivel)
-Values (1960,'Preto',null,null,'Carro','Ford','Mustang','DUAS','ABC123',50000,10000,'DISPONIVEL','ESPORTIVO','GASOLINA')
+Values (1960,'Preto',null,CURRENT_DATE,'Carro','Ford','Mustang','DUAS','ABC123',50000,10000,'DISPONIVEL','ESPORTIVO','GASOLINA')
